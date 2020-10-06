aa3ae4cccd71b99a1a6d85b1d4ea205192b2cb04
Md Anwar Jamal <cse.mdanwarjamal@gmail.com>
2020-10-06 18:30:44 +0530

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -7,8 +7,8 @@ const Navbar = ({handleChange,handleSubmit}) => {
         <div>
             <nav className="navbar fixed-top navbar-dark bg-dark">
                 <Link to="/" className="navbar-brand">DummyTube</Link>
-                <form className="form-inline" onSubmit={(e)=>handleSubmit(e)}>
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={(e)=>handleChange(e)}/>
+                <form className="form-inline" onSubmit={handleSubmit}>
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" onChange={handleChange}/>
                     <Link to="/videos" ><button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search Video</button></Link>
                 </form>
             </nav>
